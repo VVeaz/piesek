8fec1e92d0161c60f24b7ff2a044a64f4a35bf8d
Karolina <karolina_wlaz@wp.pl>
2020-03-09 13:45:24 +0100

    Stworzenie panelu bocznego dla niezalogowanej osoby

---
+++ src/components/SideMenuUnlogged.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Menu, Container, Image } from "semantic-ui-react";
+import icon from "../logo.png";
+
+export default class SideMenu extends Component {
+    state = {};
+    menuStyle = {
+        border: 0,
+        boxShadow: "none",
+        fontSize: 20,
+        backgroundColor: "#CAE2FF",
+        height: "100%",
+        width: "100%"
+    };
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+    render() {
+        const { activeItem } = this.state;
+
+        return (
+            <Menu vertical style={this.menuStyle} height={window.innerHeight}>
+                <Container>
+                    <Menu.Item>
+                        <Image src={icon}></Image>
+                        <Menu.Menu>
+                            <Menu.Item
+                                name="Zaloguj się"
+                                active={activeItem === "Zaloguj się"}
+                                onClick={this.handleItemClick}
+                            />
+                        </Menu.Menu>
+                    </Menu.Item>
+
+                </Container>
+            </Menu>
+        );
+    }
+}
