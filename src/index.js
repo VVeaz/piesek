34c091349b161dc2aab547b98f1000852226e8c9
Karolina <karolina_wlaz@wp.pl>
2020-03-29 15:11:32 +0200

    Rozwiazanie konfliktu przed mergem bo potrzebuje logowania Rafala

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router } from 'react-router-dom'
 import App from "./App";
 import LogIn from "./LogIn"
-import * as serviceWorker from "./serviceWorker";
-import axios from 'axios';
+import RegistrationUser from "./RegistrationUser"
 import "./index.css";
-import AppUnlogged from "./AppUnlogged";
-import RegistrationAdmin from "./RegistrationAdmin";
+import history from './history';
+import axios from 'axios';
+
+axios.defaults.headers.common['Authorization'] = localStorage.getItem('Authorization');
 
 const routing = (
-    <Router>
+    <Router history={history}>
         <div>
-            <Route exact path="/" component={AppUnlogged} />
+            <Route exact path="/" component={App} />
             <Route exact path="/login" component={LogIn} />
-            <Route exact path="/logged" component={App} />
-            <Route exact path="/register" component={RegistrationAdmin} />
+            <Route exact path="/RU" component={RegistrationUser} />
         </div>
     </Router>
 )
 
-axios.defaults.baseURL = 'localhost:8080/api/';
+
 ReactDOM.render(routing, document.getElementById('root'));
