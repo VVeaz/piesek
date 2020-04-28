f20316fa9089a7310ab8893ad26d776561d51512
Karolina <karolina_wlaz@wp.pl>
2020-04-28 11:58:16 +0200

    Stworzenie pliku gdzie w przyszlosci bedzie pokazywac sie katalog zwierzat, dodanie linku do strony dodawania

---
+++ src/AnimalsFolder.js
@@ -0,0 +1,24 @@
+import "semantic-ui-css/semantic.min.css";
+import SideMenu from "./components/SideMenu";
+import { Button, Form, Grid } from "semantic-ui-react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import AppUnlogged from "./AppUnlogged";
+
+class AnimalsFolder extends Component {
+
+    render() {
+        // if (!axios.defaults.headers.common["Authorization"]) {
+        //     return (<AppUnlogged />);
+        // }
+        return (
+            <Link to="/add-animal">
+                <label>Dodaj zwierze</label>
+            </Link>
+        );
+    }
+}
+export default AnimalsFolder;
+
+
