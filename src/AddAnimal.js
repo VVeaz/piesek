f0faf2fc9966b43a9c90d31fd248e287c1f4dd58
RafalLopaciuk <rafallopaciukk@gmail.com>
2020-04-28 19:53:30 +0200

    Połącznienia frontendu podstrony dodawania zwierzęcia z backendem obsługującym dodawanie do bazy danych.

--- src/AddAnimal.js
+++ src/AddAnimal.js
@@ -8,15 +8,40 @@ import AppUnlogged from "./AppUnlogged";
 class AddAnimal extends Component {
     constructor(props) {
         super(props);
-        this.state = { show: false, name: "", weight: "", birthDate: "", spicies: "", correct: false, success: false, error: false }
+        this.state = { show: false, name: "", weight: "", birthDate: "", description: "", spicies: "", birthDateApproximated: false, correct: false, success: false, error: false }
     }
 
+    componentDidUpdate() {
+        if (!this.state.correct && this.state.name.length > 0) {
+            this.setState({ correct: true })
+        }
+        else if (this.state.correct && this.state.name.length == 0) {
+            this.setState({ correct: false })
+        }
+    }
 
+    onSubmit = e => {
+        var self = this;
+        axios.post('http://localhost:8080/api/animal', {
+            "name": this.state.name,
+            "weight": this.state.weight,
+            "birthDate": this.state.birthDate,
+            "description": this.state.description,
+            "spicies": this.state.spicies,
+            "birthDateApproximated": this.state.birthDateApproximated
+        }).then(function (response) {
+            self.setState({ success: true, error: false, correct: false, name: "" })
+            console.log(response);
+        }).catch(function (error) {
+            self.setState({ error: true, correct: false })
+            console.log(error);
+        });
+    }
 
     render() {
-        // if (!axios.defaults.headers.common["Authorization"]) {
-        //     return (<AppUnlogged />);
-        // }
+        if (!axios.defaults.headers.common["Authorization"]) {
+            return (<AppUnlogged />);
+        }
         return (
             <div style={{ height: "100%" }}>
                 <Grid style={{ height: "100%", padding: 0, margin: 0 }}>
@@ -29,13 +54,13 @@ class AddAnimal extends Component {
                             <hr />
                             <Grid columns={3}>
                                 <Grid.Column>
-
-                                    <Form onSubmit={this.onSubmit} >
+                                    <p style={{ display: this.state.success ? "block" : "none", color: "green" }}>Udane dodanie.</p>
+                                    <p style={{ display: this.state.error ? "block" : "none", color: "red" }}>Problem z dodaniem zwierzęcia.</p>
+                                    <Form onSubmit={this.onSubmit}>
                                         <div class="equal width fields">
                                             <label style={{ marginRight: 5 }}>Imię</label>
                                             <tr />
                                             <input value={this.state.name} onChange={e => this.setState({ name: e.target.value })} placeholder='' />
-
                                         </div>
                                         <div class="equal width fields">
                                             <label style={{ marginRight: 5 }}>Waga</label>
@@ -43,23 +68,22 @@ class AddAnimal extends Component {
                                         </div>
                                         <div class="equal width fields">
                                             <label style={{ marginRight: 5 }}>Data urodzenia</label>
-                                            <input type="date" value={this.state.birthDate} onChange={e => this.setState({ passwordConfirm: e.target.birthDate })} placeholder='' />
+                                            <input type="date" value={this.state.birthDate} onChange={e => this.setState({ birthDate: e.target.value })} placeholder='' />
                                             <div style={{ marginLeft: 5 }} class="ui toggle checkbox">
-                                                <input type="checkbox" tabindex="0" />
+                                                <input type="checkbox" tabindex="0" onClick={() => { this.setState({ birthDateApproximated: !this.state.birthDateApproximated }) }} />
                                                 <label>Czy data przybliżona</label>
                                             </div>
                                         </div>
                                         <div class="equal width fields">
                                             <label style={{ marginRight: 5 }}>Gatunek</label>
-                                            <input value={this.state.weight} onChange={e => this.setState({ spicies: e.target.value })} placeholder='' />
+                                            <input value={this.state.species} onChange={e => this.setState({ spicies: e.target.value })} placeholder='' />
                                         </div>
                                         <div >
                                             <label>Notatki</label>
-                                            <textarea></textarea>
+                                            <textarea value={this.state.description} onChange={e => this.setState({ description: e.target.value })}></textarea>
                                         </div>
-                                        <Button style={{ backgroundColor: "#CAE2FF" }} type='submit' >Zatwierdź nowe hasło</Button>
+                                        <Button style={{ backgroundColor: "#CAE2FF" }} type='submit' >Dodaj nowe zwierzęcie</Button>
                                     </Form>
-
                                 </Grid.Column>
                             </Grid>
                         </div>
