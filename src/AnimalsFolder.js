d8d62a40c199a637c3d15480666d9f941a7cadca
Karolina <karolina_wlaz@wp.pl>
2020-05-10 00:27:57 +0200

    Stworzenie podstawowego widoku przegladarki zwierzat

--- src/AnimalsFolder.js
+++ src/AnimalsFolder.js
@@ -1,21 +1,81 @@
 import "semantic-ui-css/semantic.min.css";
-//import SideMenu from "./components/SideMenu";
-//import { Button, Form, Grid } from "semantic-ui-react";
+import SideMenu from "./components/SideMenu";
+import { Button, Form, Grid } from "semantic-ui-react";
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-//import axios from 'axios'
-//import AppUnlogged from "./AppUnlogged";
+import axios from 'axios'
+import AppUnlogged from "./AppUnlogged";
 
 class AnimalsFolder extends Component {
 
     render() {
-        // if (!axios.defaults.headers.common["Authorization"]) {
-        //     return (<AppUnlogged />);
-        // }
+        if (!axios.defaults.headers.common["Authorization"]) {
+            return (<AppUnlogged />);
+        }
         return (
-            <Link to="/add-animal">
-                <label>Dodaj zwierze</label>
-            </Link>
+            <div style={{ height: "100%" }}>
+                <Grid style={{ height: "100%", padding: 0, margin: 0 }}>
+                    <Grid.Column stretched width={2} style={{ padding: 0, margin: 0 }}>
+                        <SideMenu />
+                    </Grid.Column>
+                    <Grid.Column stretched width={14}>
+                        <div>
+                            <h1>Katalog ze zwierzętami</h1>
+                            <hr />
+                            <Grid columns={2}>
+                                <Grid.Column>
+
+                                    <table class="ui sortable celled table">
+                                        <thead>
+                                            <tr>
+                                                <th>Imię</th>
+                                                <th>Gatunek</th>
+                                                <th>Data urodzenia</th>
+                                                <th>Data dodania</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Simba</td>
+                                                <td>Lew</td>
+                                                <td>27.10.2001</td>
+                                                <td>27.01.2020</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Leszek</td>
+                                                <td>Pies</td>
+                                                <td>27.10.2010</td>
+                                                <td>27.10.2019</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Misiek</td>
+                                                <td>Kot</td>
+                                                <td>27.10.2011</td>
+                                                <td>27.10.2018</td>
+                                            </tr>
+                                        </tbody>
+
+                                    </table>
+                                    <Link to="/add-animal">
+                                        <div align="right" style={{ marginTop: 10 }} >
+                                            <button class="circular ui icon button" style={{ backgroundColor: "#B2E8C4" }} type="reset" >
+                                                <i class="plus icon"></i>
+                                            </button>
+                                            <label>Dodaj zwierzę</label>
+                                        </div>
+                                    </Link>
+
+                                </Grid.Column>
+                            </Grid>
+                        </div>
+
+                    </Grid.Column>
+
+                </Grid>
+
+            </div>
+
+
         );
     }
 }
